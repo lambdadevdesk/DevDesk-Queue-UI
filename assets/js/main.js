47cf687dfbde607abc990c9befb4c9b5e910ec51
Sean Hockin <sean.hockin@gmail.com>
2019-04-26 18:58:22 -0600

    Removed functions.less. Added functionality to hamburger icon. Did some mobile responsiveness

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Grab the hamburger icon
+let hamburger = document.querySelector('header i');
+
+// Get header nav
+let headerNav = document.querySelector('header nav');
+
+hamburger.addEventListener('click', function(e) {
+    headerNav.classList.toggle('hidden');
+});
