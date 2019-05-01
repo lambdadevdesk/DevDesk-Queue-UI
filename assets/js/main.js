147f31e7060afcaf9889cd961b65e91f9e472761
Sean Hockin <sean.hockin@gmail.com>
2019-05-01 08:03:46 -0600

    About us page now dynamically generates the team members based on JS objects.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,33 @@ class TeamMember {
 
 
 //Create team members
-let sean = new TeamMember('Sean', 'Hockin', 'UI Design', 'shockin779', 'https://github.com/shockin779', '../images/sean-hockin.jpg');
-let christian = new TeamMember('Christian', 'MacDonald', 'UI Design', 'ChristianMacDonald', 'https://github.com/ChristianMacDonald', '../images/sean-hockin.jpg');
-let jeremy = new TeamMember('Jeremy', 'Morales', 'UI Design', 'performance-fleece', 'https://github.com/performance-fleece', '../images/sean-hockin.jpg');
-let suen = new TeamMember('Suen', 'O', 'Front-End Architect', 'Suen-O', 'https://github.com/Seun-O', '../images/sean-hockin.jpg');
-let mychal = new TeamMember('Mychal', 'Hall', 'Front-End Architect', 'mychal-hall', 'https://github.com/mychal-hall', '../images/sean-hockin.jpg');
-let xavier = new TeamMember('Xavier', 'Puentes', 'Back-End Architect', 'xpuentes', 'https://github.com/xpuentes', '../images/sean-hockin.jpg');
+let sean = new TeamMember('Sean', 'Hockin', 'UI Design', 'shockin779', 'https://github.com/shockin779', 'assets/images/sean-hockin.jpg');
+let christian = new TeamMember('Christian', 'MacDonald', 'UI Design', 'ChristianMacDonald', 'https://github.com/ChristianMacDonald', 'assets/images/sean-hockin.jpg');
+let jeremy = new TeamMember('Jeremy', 'Morales', 'UI Design', 'performance-fleece', 'https://github.com/performance-fleece', 'assets/images/sean-hockin.jpg');
+let suen = new TeamMember('Suen', 'O', 'Front-End Architect', 'Suen-O', 'https://github.com/Seun-O', 'assets/images/sean-hockin.jpg');
+let mychal = new TeamMember('Mychal', 'Hall', 'Front-End Architect', 'mychal-hall', 'https://github.com/mychal-hall', 'assets/images/sean-hockin.jpg');
+let xavier = new TeamMember('Xavier', 'Puentes', 'Back-End Architect', 'xpuentes', 'https://github.com/xpuentes', 'assets/images/sean-hockin.jpg');
 
 let team = [sean, christian, jeremy, suen, mychal, xavier];
+
+
+// Grab the members div
+let membersSection = document.querySelector('.members');
+
+
+team.forEach(function(member) {
+
+    let tempDiv = document.createElement('div');
+    tempDiv.classList.add('team-member');
+    tempDiv.innerHTML = `<img src="${member.image}" alt="Picture of team member ${member.firstName} ${member.lastName}">
+    <div class="member-info">
+        <h3>${member.firstName} ${member.lastName}</h3>
+        <p>${member.role}</p>
+        <p>Github Handle: <a href="${member.githubLink}" target="_blank">${member.githubUsername}</a></p>`;
+
+    membersSection.append(tempDiv);
+})
+
 
 // Grab the hamburger icon
 let hamburger = document.querySelector('header i');
